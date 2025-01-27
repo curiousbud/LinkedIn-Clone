ea3dd2a522fa29638af178d9ca3eddaf9e3148ed
Areeb <akareeb662@gmail.com>
2025-01-27 22:34:01 +0530

    Updated whole codebase

--- LinkedIn-Clone/backend/server.js
+++ LinkedIn-Clone/backend/server.js
@@ -1,18 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
-
-import authRoutes from "./routes/auth.route.js";
-
-import { connectDB } from "./lib/db.js";
-
-dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/v1/auth", authRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  connectDB();
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.error(err));
